5a78b84265e655c1ec7ac19aa5ed303907fae489
Hleb Bandarenka <hleb.bandarenka@google.com>
2014-10-31 22:12:54 +0300

    Part_10

--- web/front.php
+++ web/front.php
@@ -8,31 +8,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
-
-function render_template($request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
-
 $dispatcher = new EventDispatcher();
-$dispatcher->addListener('response', array(new Simplex\ContentLengthListener(), 'onResponse'), -255);
-$dispatcher->addListener('response', array(new Simplex\GoogleListener(), 'onResponse'));
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
 
-$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
+$listener = new HttpKernel\EventListener\ExceptionListener('Calendar\\Controller\\ErrorController::exceptionAction');
+$dispatcher->addSubscriber($listener);
+$dispatcher->addSubscriber(new Simplex\StringResponseListener());
+
+$framework = new Simplex\Framework($dispatcher, $resolver);
 
 use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 use Symfony\Component\HttpKernel\HttpCache\Store;
